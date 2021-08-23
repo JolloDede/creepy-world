d99eb0f99101861d342be5dcd60144c5961c1079
JolloDede <jollodede@gmail.com>
2021-08-23 19:04:02 +0200

    dunno

---
+++ src/manager/dragManager.ts
@@ -0,0 +1,22 @@
+import { Structure } from "../chars/structure";
+
+/**
+ * DragManager manages the Element your holding in your hand a the time
+ */
+export class DragManager {
+    private structure: Structure;
+
+    constructor() {
+
+    }
+
+    holdStructure() {
+        this.structure = 
+    }
+}
+
+export enum Structures {
+    Collector,
+    Blaster,
+    
+}
