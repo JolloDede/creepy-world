7e5dd52bfadc0f4b1536a31f301c704aae0b64e3
JolloDede <jollodede@gmail.com>
2021-08-23 20:00:55 +0200

    refactor: gamecontrolls
    im starting from scratch and build up a new game overlay

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div className="m-0 p-0">
+      <div className="row w-100">
+        <canvas className="p-0 w-100"></canvas>
+      </div>
+      <div className="row w-100">
+        <div className="col-5">
+          <h2>Structure Buttons</h2>
+        </div>
+        <div className="col-5">
+          <h2>Other things</h2>
+        </div>
+        <div className="col-2">
+          <h2>Options</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
