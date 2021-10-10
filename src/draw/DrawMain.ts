b0c0d116c7ec36065bd90737f468d2fb96b40a1d
JolloDede <jollodede@gmail.com>
2021-10-10 13:08:07 +0200

    feat: map
    drawing the map of the game

--- src/draw/DrawMain.ts
+++ src/draw/DrawMain.ts
@@ -4,6 +4,7 @@ export default class DrawMain {
     width: number;
     height: number;
     map: GameMap;
+    g: CanvasRenderingContext2D | undefined;
 
     constructor(map: GameMap) {
         this.width = 0;
@@ -11,29 +12,33 @@ export default class DrawMain {
         this.map = map;
     }
 
-    draw(g: CanvasRenderingContext2D) {
+    render() {
+        if (this.g === undefined) return;
         let pixelWidth = Math.round(this.width / 70);
         let pixelHeight =  Math.round(this.height / 48);
 
-        for (let i = 0; i < 5/*this.map.dimensions.y */; i++) {
+        for (let i = 0; i < this.map.dimensions.y; i++) {
             for (let ii = 0; ii < this.map.dimensions.x; ii++) {
+                this.g.beginPath();
+                this.g.strokeStyle = "#FFFFFF";
+                this.g.strokeRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
                 switch (this.map.map[i][ii]) {
                     case 0:
-                        g.beginPath();
-                        g.fillStyle = "#000000";
-                        g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
+                        this.g.beginPath();
+                        this.g.fillStyle = "#000000";
+                        this.g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
                         break;
 
                     case 1:
-                        g.beginPath();
-                        g.fillStyle = "#FF0000";
-                        g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
+                        this.g.beginPath();
+                        this.g.fillStyle = "#FF0000";
+                        this.g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
                         break;
 
                     case 2:
-                        g.beginPath();
-                        g.fillStyle = "#008000";
-                        g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
+                        this.g.beginPath();
+                        this.g.fillStyle = "#008000";
+                        this.g.fillRect(pixelWidth*ii, pixelHeight*i, pixelWidth, pixelHeight);
                         break;
 
                     default:
@@ -46,5 +51,9 @@ export default class DrawMain {
     setWidthHeight(width: number, height: number) {
         this.width = width;
         this.height = height;
+    }
+
+    setRenderContext(g: CanvasRenderingContext2D) {
+        this.g = g;
     }
 }
