d6938b9fd03624be120411ffc97f1ef2c646234c
JolloDede <jollodede@gmail.com>
2021-12-16 13:52:49 +0100

    refactor: ===

--- src/helper/Connection.ts
+++ src/helper/Connection.ts
@@ -17,14 +17,14 @@ export class Connections {
         let found = false;
         for (let i = 0; i < this.connections.length; i++) {
             const connection = this.connections[i];
-            if (connection.a.x == newConnection.a.x && connection.a.y == newConnection.a.y) {
-                if (connection.b.x == newConnection.b.x && connection.b.y == newConnection.b.y) {
+            if (connection.a.x === newConnection.a.x && connection.a.y === newConnection.a.y) {
+                if (connection.b.x === newConnection.b.x && connection.b.y === newConnection.b.y) {
                     found = true;
                     break;
                 }
             }
-            if (connection.a.x == newConnection.b.x && connection.a.y == newConnection.b.y) {
-                if (connection.b.x == newConnection.a.x && connection.b.y == newConnection.a.y) {
+            if (connection.a.x === newConnection.b.x && connection.a.y === newConnection.b.y) {
+                if (connection.b.x === newConnection.a.x && connection.b.y === newConnection.a.y) {
                     found = true;
                     break;
                 }
