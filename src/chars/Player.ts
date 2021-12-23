4c341ab7d9cb989defd682d4dacb37333ce54b17
JolloDede <jollodede@gmail.com>
2021-12-23 10:10:41 +0100

    feat: energy display

--- src/chars/Player.ts
+++ src/chars/Player.ts
@@ -8,17 +8,21 @@ export default class Player extends Building {
         super(new Point(x, y), game);
         this.health = 40;
         this.maxHealth = 40;
-        this.setEnergy(10);
         this.maxEnergy = 40;
+        this.setEnergy(10);
 
         this.built = true;
         this.size = 5;
     }
 
-    
+
     setEnergy = (val: number) => {
+        let ele = document.getElementById("energy-progress");
+        // val
+        ele!.innerText = val.toString() + "/" + this.maxEnergy.toString();
+        // progressbar
+        ele!.style.width = (100 / this.maxEnergy * val).toString() + "%";
+
         this.energy = val;
-        // todo make it to a progress bar
-        document.getElementById("energy-progress")!.innerText = this.energy.toString();
     }
 }
